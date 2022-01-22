1c09ecb7b7f7da06e425eb63167902b6bf4c8e4c
Scott Park <scott.park.dev@gmail.com>
2022-01-21 17:57:55 -0800

    Added some css files

---
+++ src/landing.js
@@ -0,0 +1,7 @@
+//Landing Page
+import './landing.css';
+// First Build HTML
+
+const main = document.createElement('main');
+const header = document.createElement('header');
+const footer = document.createElement('footer');
