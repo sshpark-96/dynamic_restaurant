cffe5453b57d85d8dfeadcccc92d86656bb1dbf6
Scott Park <scott.park.dev@gmail.com>
2022-01-27 14:33:38 -0800

    Made the tabs work

--- src/index.js
+++ src/index.js
@@ -6,13 +6,23 @@ import { contact } from "./pages/contact";
 import './css/index.css';
 
 const tab_triggers = document.querySelectorAll('.tab_link');
+
 tab_triggers.forEach(tab =>{
-    tab.addEventListener('click', ()=>{
-        let current_tab = document.querySelector(`.link[data-tab-content = "'${tab.dataset.tabTrigger}'"]`);
-        console.log(tab.textContent);
-        document.querySelector('.list.active').classList.remove('active');
+    console.log(tab.textContent);
+    tab.addEventListener('click',()=>{
+        let tab_number = tab.dataset.tabTrigger;
+        let tab_content = document.querySelector(`.tab_content[data-tab-content = "${tab_number}"]`);
+        document.querySelector('.tab_link.active').classList.remove('active');
         document.querySelector('.tab_content.open').classList.remove('open');
-        current_tab.classList.add('open');
         tab.classList.add('active');
+        tab_content.classList.add('open');
     })
+    // tab.addEventListener('click', ()=>{
+    //     let current_tab = document.querySelector(`.link[data-tab-content = "'${tab.dataset.tabTrigger}'"]`);
+    //     console.log(tab.textContent);
+    //     document.querySelector('.list.active').classList.remove('active');
+    //     document.querySelector('.tab_content.open').classList.remove('open');
+    //     current_tab.classList.add('open');
+    //     tab.classList.add('active');
+    // })
 })
